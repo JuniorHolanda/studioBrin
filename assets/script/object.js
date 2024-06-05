ff34f338a4f221256d7fea833ce34ed94f157047
Júnor Holanda <139183744+JuniorHolanda@users.noreply.github.com>
2024-06-05 16:49:08 -0300

    ajustes no layout
    ajustes no layout de botões de comtrole e tambem calibrei as cores

--- assets/script/object.js
+++ assets/script/object.js
@@ -19,6 +19,7 @@ class MoldProduct {
 // Lista com os produtos para serem iterados, as pastas com as partes do produtos devem ser nomeadas em camelCase e identica aos itens deste array, AJUSTE DENTRO DAS CONDIÇÕES DA REPETIÇÃO
 const listProduct = [
     'bolsaPraiaDuo',
+    'bolsaTripCongresso',
     'bolsaPst',
     'estojoTriang',
     'miniBagVani',
@@ -28,22 +29,24 @@ const listProduct = [
 
 // sempre que adicionar um produto é necessário adicionar a medida, seguindo a ordem cronológica
 const listMedidas = [
-    'Lar - 10cm | Alt - 00cm | Comp - 00cm', /*bolsaPraiaDuo*/
-    'Lar - 20cm | Alt - 00cm | Comp - 00cm', /*bolsaPst*/
-    'Lar - 30cm | Alt - 00cm | Comp - 00cm', /*estojoTriang*/
-    'Lar - 40cm | Alt - 00cm | Comp - 00cm', /*miniBagVani*/
-    'Lar - 50cm | Alt - 00cm | Comp - 00cm', /*necessaireEvBolso*/
-    'Lar - 60cm | Alt - 00cm | Comp - 00cm', /*termicaJl*/
+    'Lar - 10cm | Alt - 00cm | Comp - 00cm', //bolsaPraiaDuo
+    'Lar - 10cm | Alt - 00cm | Comp - 00cm', //bolsaTripCongresso
+    'Lar - 20cm | Alt - 00cm | Comp - 00cm', //bolsaPst
+    'Lar - 30cm | Alt - 00cm | Comp - 00cm', //estojoTriang
+    'Lar - 40cm | Alt - 00cm | Comp - 00cm', //miniBagVani
+    'Lar - 50cm | Alt - 00cm | Comp - 00cm', //necessaireEvBolso
+    'Lar - 60cm | Alt - 00cm | Comp - 00cm', //termicaJl
 ]
 
 // assim como as medidas, essa lista precisa ser preenchida na ordem em que novos produtos são adicionados
 const listInfoProduct = [
-    'Uma Bolsa perfeita para todas ocasiões', /*bolsaPraiaDuo*/
-    'esta é outra bolsa', /*bolsaPst*/
-    'este é um estojo', /*estojoTriang*/
-    'e isso uma minibag', /*miniBagVani*/
-    'essa necessaire', /*necessaireEvBolso*/
-    'e essa é uma térmica', /*termicaJl*/
+    'Uma Bolsa perfeita para todas ocasiões', //bolsaPraiaDuo
+    'Uma Bolsa perfeita para todas ocasiões', //bolsaTripCongresso
+    'esta é outra bolsa', //bolsaPst
+    'este é um estojo', //estojoTriang
+    'e isso uma minibag', //miniBagVani
+    'essa necessaire', //necessaireEvBolso
+    'e essa é uma térmica', //termicaJl
 ]
 
 // lista com as categorias dos objetos criados na iteração.
@@ -63,7 +66,6 @@ for (let i = 0; i < listProduct.length; i++){
     const nome = listProduct[i];
     const medida = listMedidas[i]
     const info = listInfoProduct[i]
-
     const ajuste = `assets/img/${nome}/ajuste.jpg`;
     const corpo = `assets/img/${nome}/corpo.png`;
     const card = `assets/img/${nome}/card.jpg`
@@ -78,12 +80,13 @@ for (let i = 0; i < listProduct.length; i++){
     let base = '';
 
     // adiciona alça aos aos produtos relativos
-    if (nome === 'bolsaPraiaDuo' || nome === 'bolsaPst' || nome === 'miniBagVani' || nome === 'termicaJl'){
+    if (
+        nome === 'bolsaPraiaDuo' || nome === 'bolsaTripCongresso' || nome === 'bolsaPst' || nome === 'miniBagVani' || nome === 'termicaJl'){
         alca = `assets/img/${nome}/alca.png`
     }
 
     // adiciona ziper aos aos produtos relativos
-    if (nome === 'estojoTriang' || nome === 'miniBagVani' || nome === 'necessaireEvBolso' || nome === 'termicaJl' ){
+    if (nome === 'estojoTriang' || nome === 'bolsaTripCongresso' || nome === 'miniBagVani' || nome === 'necessaireEvBolso' || nome === 'termicaJl' ){
         ziper = `assets/img/${nome}/ziper.png`
     }
 
@@ -103,7 +106,7 @@ for (let i = 0; i < listProduct.length; i++){
     }
 
     // adiciona tampa aos aos produtos relativos
-    if (nome === 'termicaJl'){
+    if (nome === 'bolsaTripCongresso' || nome === 'termicaJl' ){
        tampa = `assets/img/${nome}/tampa.png`
     }
 
